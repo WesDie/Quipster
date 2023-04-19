1fea27a8955e5dae0bacd204843f708c4aa1eab0
l.j <59925222+lokejonatan@users.noreply.github.com>
2023-04-19 11:40:18 +0200

    added toggle buttons js incoming

---
+++ site/chat.js
@@ -0,0 +1,10 @@
+// document.getElementById("toggleLeft").addEventListener("click", Toggle(0));
+// document.getElementById("toggleRight").addEventListener("click", Toggle(1));
+
+function Toggle(side) {
+    if (side) {
+
+    } else {
+        document.getElementById("toggleLeft").innerHTML = "chevron_right";
+    }
+}
--- site/chat.php
+++ site/chat.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="UTF-8" />
-  <title>Hello, world!</title>
+  <title>Quipster APP</title>
   <meta name="viewport" content="width=device-width,initial-scale=1" />
   <meta name="description" content="" />
   <link rel="stylesheet" type="text/css" href="style.css" />
@@ -11,6 +11,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
   <link rel="stylesheet" href="chat.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
+  <script src="chat.js"></script>
 </head>
 
 <body>
@@ -29,6 +30,9 @@
       <div class="chat selected">
         <img src="/images/box.png" alt="">
         <p>das</p>
+        <button class="material-symbols-outlined">
+          more_horiz
+        </button>
       </div>
       <?php
       for ($i = 0; $i < 20; $i++) {
@@ -49,6 +53,17 @@
     </div>
   </div>
   <div class="middle">
+    <div class="top">
+      <button onclick="Toggle(1)" id="toggleLeft" class="material-symbols-outlined">
+        chevron_left
+      </button>
+      <p style="display: inline;">
+        chat title
+      </p>
+      <button id="toggleRight" class="material-symbols-outlined" style="float: right;">
+        chevron_right
+      </button>
+    </div>
     <div class="currentchat">
       blablas
     </div>
