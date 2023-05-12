cdb4bb92419d7d05147927c30269fcaadca799fe
l.j <59925222+lokejonatan@users.noreply.github.com>
2023-05-12 11:01:40 +0200

    now the user has to be logged in to post

--- site/dbquery.php
+++ site/dbquery.php
@@ -2,51 +2,56 @@
 // need to check if the current logged in user has permission to the chat that they are trying to post messaage
 require_once('db.php');
 
-if ($_POST['new'] == 'true') {
-    // expect two variables:
+session_start();
 
-    $id = uniqid();
-    $stmtId = $conn->prepare("SELECT id FROM messages WHERE id = '$id'");
-    $stmtId->execute();
+if (isset($_SESSION['logedin']) && $_SESSION['logedin']) {
 
-    while ($stmtId->rowCount() > 0) {
+    if ($_POST['new'] == 'true') {
+        // expect two variables:
+
         $id = uniqid();
         $stmtId = $conn->prepare("SELECT id FROM messages WHERE id = '$id'");
         $stmtId->execute();
-    }
 
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("INSERT INTO messages (id, chat, user, message) VALUES (:id, 'dev_chat', '645b6d4d40d17', :message)");
-    $stmt->bindParam(':id', $id);
-    // $stmt->bindParam(':chat', $id);
-    $user = "6447906d0b6be";
-    // $stmt->bindParam(':user', $user);
-    // $stmt->bindParam(':type', $id);
-    $stmt->bindParam(':message', $id);
-    // $stmt->bindParam(':replyto', $id);
+        while ($stmtId->rowCount() > 0) {
+            $id = uniqid();
+            $stmtId = $conn->prepare("SELECT id FROM messages WHERE id = '$id'");
+            $stmtId->execute();
+        }
 
-    // $id = $key;
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $stmt = $conn->prepare("INSERT INTO messages (id, chat, user, message) VALUES (:id, 'dev_chat', :user, :message)");
+        $stmt->bindParam(':id', $id);
+        // $stmt->bindParam(':chat', $id);
+        $user = $_SESSION['id'];
+        $stmt->bindParam(':user', $user);
+        // $stmt->bindParam(':type', $id);
+        $stmt->bindParam(':message', $id);
+        // $stmt->bindParam(':replyto', $id);
 
-    if ($stmt->execute()) {
+        // $id = $key;
+
+        if ($stmt->execute()) {
 
 
-        echo json_encode(array("success"));
-    }
-} else {
+            echo json_encode(array("success"));
+        }
+    } else {
 
-    $stmt = $conn->prepare("SELECT * FROM messages WHERE chat = 'dev_chat'");
+        $stmt = $conn->prepare("SELECT * FROM messages WHERE chat = 'dev_chat'");
 
-    if ($stmt->execute()) {
-        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        $newMsgs = $stmt->fetchAll();
+        if ($stmt->execute()) {
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $newMsgs = $stmt->fetchAll();
 
-        $lastLoaded = $_POST['lastLoaded'];
+            $lastLoaded = $_POST['lastLoaded'];
 
-        $sql = "SELECT * FROM messages WHERE sent >= '$lastLoaded'";
+            $sql = "SELECT * FROM messages WHERE sent >= '$lastLoaded'";
 
-        // $newMsgs = array(
-        //     $sql
-        // );
-        echo json_encode($newMsgs);
+            // $newMsgs = array(
+            //     $sql
+            // );
+            echo json_encode($newMsgs);
+        }
     }
 }
