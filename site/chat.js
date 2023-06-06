d6d691a5b3247a1841d4a274ef4453b186c8485c
l.j <59925222+lokejonatan@users.noreply.github.com>
2023-06-06 13:47:53 +0200

    fixed huge no go

--- site/chat.js
+++ site/chat.js
@@ -286,7 +286,7 @@ function UpdateMessages(lastLoaded, chat_id) {
 
 
                 const tekst = message.appendChild(document.createElement("p"))
-                tekst.innerHTML = element.message;
+                tekst.textContent = element.message;
 
                 message.classList.add("message");
                 currentchat.appendChild(message);
