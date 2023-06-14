b5a1833b3f62f5fa45c8c780399f9d2d106450fc
l.j <59925222+lokejonatan@users.noreply.github.com>
2023-06-14 10:20:05 +0200

    fixed duplicate code

--- site/chat.js
+++ site/chat.js
@@ -9,7 +9,8 @@ const mqLarge = window.matchMedia('(min-width: 1200px)');
 let leftOpen = true, rightOpen = false, left = "0", right = "0";
 
 function Small() {
-    windowD = "small"; console.log('small');
+    windowD = "small";
+    // console.log('small');
     document.getElementsByTagName("body")[0].style.gridTemplateColumns = 0 + " 1fr " + 0;
 
     document.getElementById("toggleLeft").innerHTML = "chevron_right";
@@ -26,7 +27,8 @@ function Small() {
     document.getElementsByTagName("body")[0].style.gridTemplateColumns = left + " 1fr " + 0;
 }
 function Medium() {
-    windowD = "medium"; console.log('medium');
+    windowD = "medium";
+    // console.log('medium');
 
     document.getElementById("toggleLeft").innerHTML = "chevron_left";
     left = "auto";
@@ -42,7 +44,8 @@ function Medium() {
     document.getElementsByTagName("body")[0].style.gridTemplateColumns = left + " 1fr " + right;
 }
 function Large() {
-    windowD = "large"; console.log('large');
+    windowD = "large";
+    // console.log('large');
 
     document.getElementById("toggleLeft").innerHTML = "chevron_left";
     left = "auto";
@@ -98,7 +101,6 @@ function Toggle(side) {
                 leftOpen = true;
                 document.getElementById("left").removeAttribute("inert", "");
             } else {
-                console.log("yes");
                 document.getElementById("toggleLeft").innerHTML = "chevron_left";
                 left = "auto";
                 leftOpen = true;
@@ -342,7 +344,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // change chat
     function ChangeChat(nextChat) {
-        console.log("hey " + nextChat);
+        // console.log("hey " + nextChat);
         window.chat = nextChat;
         $(".list .list-item").removeClass("selected");
         $("div[data-id='" + nextChat + "']").addClass("selected");
@@ -439,7 +441,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     function sendFriendRequest(userid) {
         let queryString = 'action=friendRequest' + '&userid=' + userid;
-        // console.log(queryString);
+
         $.ajax({
             url: "dbquery.php",
             data: queryString,
@@ -453,6 +455,10 @@ document.addEventListener("DOMContentLoaded", function () {
                 console.log(error);
             }
         });
+
+        $("#user-profile div:nth-child(4) button:nth-child(1)").attr("disabled", true);
+        $("#user-profile div:nth-child(4) button:nth-child(1)").attr('onclick', false);
+        $("#user-profile div:nth-child(4) button:nth-child(1)").text("Already sent friende request!");
     }
     function UpdateMessages(lastLoaded, chat_id) {
         let queryString = 'action=chatLoad' + '&chat_id=' + chat_id + '&lastLoaded=' + lastLoaded;
@@ -564,7 +570,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     function ContextMenu(e) {
-        console.log('das');
         // e.preventDefault();
         contextMenu.style.display = "none";
 
@@ -724,102 +729,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-// change chat
-function ChangeChat(nextChat) {
-    window.chat = nextChat;
-    $(".list .list-item").removeClass("selected");
-    $("div[onclick=\"ChangeChat('" + nextChat + "')\"]").addClass("selected");
-    clearInterval(intervalUpdateMessages);
-    $("#currentchat .message").remove();
-    document.getElementById("memberList").innerHTML = '';
-    UpdateMembers(window.chat);
-    lastLoadedX = 0;
-    intervalUpdateMessages = setInterval(function () {
-        UpdateMessages(lastLoadedX, window.chat);
-    }, 1000);
-}
 
-// send message
-function SendMessage() {
-    let input = document.querySelector("#newMessage input").value, chat_id = window.chat;
-
-    let queryString = 'action=chatUpload' + '&chat_id=' + chat_id + '&input=' + input;
-    // console.log(queryString);
-    $.ajax({
-        url: "dbquery.php",
-        data: queryString,
-        type: "POST",
-        dataType: "json",
-        success: function (response) {
-            // console.log("post success");
-            // console.log(response);
-            document.querySelector("#newMessage input").value = "";
-            UpdateMessages(lastLoadedX, chat_id)
-        },
-        error: function (error) {
-            console.log("post error");
-            console.log(error);
-        }
-    });
-}
-$(document).ready(function () {// when the dom is loaded
-    // send message when enter released:
-    $("#inpurt").on("keyup", function (event) {
-        if (event.which === 13) {
-            SendMessage();
-        }
-    });
-});
-// update messages in current chat:
-
-// let loadedMsgs = new array();
-
-var lastLoadedX = "1999";
-
-// setInterval(function () {
-//     UpdateMessages(lastLoadedX, "dev_chat");
-// }, 1000);
-function UpdateMembers(chat_id) {
-    let queryString = 'action=updateMembers' + '&chat_id=' + chat_id;
-
-    $.ajax({
-        url: "dbquery.php",
-        data: queryString,
-        type: "POST",
-        dataType: "json",
-        success: function (response) {
-            document.getElementById("memberList").innerHTML = '';
-            //succes
-            response.forEach(element => {
-                // console.log("added member");
-                const userProfile = document.createElement("div");
-                userProfile.setAttribute("class", "list-item");
-                userProfile.setAttribute("data-id", element.id);
-                userProfile.classList.add("showProfile");
-                const pfpProfile = userProfile.appendChild(document.createElement("img"));
-                pfpProfile.setAttribute("src", element.pfp);
-                const profileUsername = userProfile.appendChild(document.createElement("p"));
-                profileUsername.innerHTML = element.username;
-                const profileButton = userProfile.appendChild(document.createElement("button"));
-                profileButton.setAttribute("class", "material-symbols-outlined showProfile");
-                profileButton.setAttribute("data-id", element.id);
-                profileButton.innerHTML = "more_horiz";
-
-                // profileButton.innerHTML = "more_horiz"
-
-                document.getElementById("memberList").appendChild(userProfile);
-
-                if (element.status == "online") {
-                    pfpProfile.setAttribute("class", "onlinePfp");
-                }
-            });
-        },
-        error: function (error) {
-            console.log("post error");
-            console.log(error);
-        }
-    });
-}
 
 function UpdateNotifications() {
 
@@ -872,112 +782,3 @@ function UpdateNotifications() {
         }
     });
 }
-
-function sendFriendRequest(userid) {
-    let queryString = 'action=friendRequest' + '&userid=' + userid;
-
-    $.ajax({
-        url: "dbquery.php",
-        data: queryString,
-        type: "POST",
-        dataType: "json",
-        success: function (response) {
-
-        },
-        error: function (error) {
-            console.log("post error");
-            console.log(error);
-        }
-    });
-
-    $("#user-profile div:nth-child(4) button:nth-child(1)").attr("disabled", true);
-    $("#user-profile div:nth-child(4) button:nth-child(1)").attr('onclick', false);
-    $("#user-profile div:nth-child(4) button:nth-child(1)").text("Already sent friende request!");
-}
-function UpdateMessages(lastLoaded, chat_id) {
-    if (chat_id) {
-        chat_id = "dev_chat";
-    }
-    let queryString = 'action=chatLoad' + '&chat_id=' + chat_id + '&lastLoaded=' + lastLoaded;
-    // console.log(queryString);
-    $.ajax({
-        url: "dbquery.php",
-        data: queryString,
-        type: "POST",
-        dataType: "json",
-        success: function (response) {
-            // console.log("update success");
-            // console.log(response);
-            /*  const currentchat = document.getElementById("currentchat");
-              response.forEach(element => {
-                  // console.log(element);
-                  // console.log(element.user);
-  
-                  const message = document.createElement("div");
-                  const pfp = message.appendChild(document.createElement("img")).setAttribute("src", element.pfp);
-                  const user = message.appendChild(document.createElement("div"))
-                  // user.attr('data-id','yoooos');
-                  user.classList.add("user");
-                  user.appendChild(document.createElement("b")).innerHTML = element.username;
-                  const details = user.appendChild(document.createElement("time"));
-                  // details.classList.add("timestamp");
-  
-                  let now = new Date();
-                  let date = new Date(element.sent);
-                  // now.toDateString();
-                  // return element.sent.toDateString() === now.toDateString()
-                  //     ? "today at ${date.toLocaleTimeString([], { timeStyle: 'short' })}"
-                  //     : "${date.toLocaleDateString()} at ${date.toLocaleTimeString([], { timeStyle: 'short' })}";
-  
-                  details.innerHTML = date.toDateString() === now.toDateString() ?
-                      `Today at ${date.toLocaleTimeString([], { timeStyle: 'short' })}`
-                      : `${date.toLocaleDateString()} at ${date.toLocaleTimeString([], { timeStyle: 'short' })}`;
-  
-  
-                  const tekst = message.appendChild(document.createElement("p"))
-                  tekst.textContent = element.message;
-  
-                  message.classList.add("message");
-                  currentchat.appendChild(message);
-                  var objDiv = document.getElementById("currentchat");
-                  objDiv.scrollTop = objDiv.scrollHeight;
-  
-                  lastLoadedX = element.sent;
-              });*/
-            const currentchat = $("#currentchat");
-            response.forEach(element => {
-                const message = $("<div></div>");
-                message.attr('data-id', element.id);
-                const pfp = $("<img>").attr("src", element.pfp).appendTo(message);
-                pfp.addClass('showProfile');
-                const user = $("<div></div>").addClass("user").appendTo(message);
-                user.attr('data-id', element.user);
-                const name = $("<b></b>").html(element.username).appendTo(user);
-                name.attr("data-id", element.user);
-                name.addClass('showProfile');
-                const details = $("<time></time>").appendTo(user);
-                let now = new Date();
-                let date = new Date(element.sent);
-                details.html(date.toDateString() === now.toDateString() ?
-                    `Today at ${date.toLocaleTimeString([], { timeStyle: 'short' })}` :
-                    `${date.toLocaleDateString()} at ${date.toLocaleTimeString([], { timeStyle: 'short' })}`);
-
-                const tekst = $("<p></p>").text(element.message).appendTo(message);
-                message.addClass("message");
-                currentchat.append(message);
-                $("#currentchat").scrollTop($("#currentchat")[0].scrollHeight);
-
-                lastLoadedX = element.sent;
-            });
-        },
-        error: function (error) {
-            console.log("update error");
-            console.log(error);
-            // console.log(lastLoadedX);
-        }
-    });
-}
-
-
-
-// reload everything else:
